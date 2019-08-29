8f90356b43ca41171a241128dfde707692bf67c5
Declan Wu <declan.s.wu@gmail.com>
2019-08-29 09:56:10 -0400

    Updates

---
+++ precache-manifest.1e826393a18a356bd4fed5af21840362.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee317a6ed245f925362d10f329574118",
+    "url": "/github.io/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f48cbde7cac338cd482d",
+    "url": "/github.io/monsters-rolodex/static/css/main.57360ffc.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/github.io/monsters-rolodex/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "f48cbde7cac338cd482d",
+    "url": "/github.io/monsters-rolodex/static/js/main.eb6c8805.chunk.js"
+  },
+  {
+    "revision": "b5e4f5b4c80e9d9039b1",
+    "url": "/github.io/monsters-rolodex/static/js/runtime~main.f5fb4d1f.js"
+  }
+]);
